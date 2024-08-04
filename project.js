e22d83adc958e5384f9dc4f34cd07ef6cac741be
oblachko88 <88oblachko@gmail.com>
2024-08-04 14:11:23 +0300

    commit Aug 4

---
+++ project.js
@@ -0,0 +1,26 @@
+// 1. Despot some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again 
+
+// function deposit() {
+//   return 1
+// }
+
+const prompt = require("prompt-sync")();
+
+
+
+const deposit = () => {
+  const depositAmount = prompt("Enter a deposit amount: ");
+  const numberDepositAmount = parseFloat(depositAmount);
+  
+  if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+    console.log("Invalid deposit amount, try again.");
+    }
+  };
+
+deposit();
